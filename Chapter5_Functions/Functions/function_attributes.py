f735e0a00145040292e2888cce48c31ca1874864
Jan Schaffranek <jan.schaffranek@rub.de>
2020-04-15 10:06:21 +0200

    add

--- Chapter5_Functions/Functions/function_attributes.py
+++
@@ -1,9 +0,0 @@
-def my_function(a, b, c=None):
-    return a + b
-
-print(dir(my_function))
-print(my_function.__dict__)
-print(my_function.__code__)
-print(my_function.__code__.co_argcount)
-print(my_function.__code__.co_varnames)
-print(my_function.__code__.co_kwonlyargcount)
