86bd037c2b5e25392c4a3ae712bb92087d40b3bf
Jan Schaffranek <jan.schaffranek@rub.de>
2020-04-20 15:22:31 +0200

    add

--- Chapter10_Cython/2_CythonCode/tests/test_vector.py
+++ Chapter10_Cython/2_CythonCode/tests/test_vector.py
@@ -2,40 +2,40 @@
 """
 import unittest
 
-from vector import Vector2D
+import fastvector
 
 class VectorTests(unittest.TestCase):
     def setUp(self):
-        self.v1 = Vector2D(0, 0)
-        self.v2 = Vector2D(-1, 1)
-        self.v3 = Vector2D(2.5, -2.5)
+        self.v1 = fastvector.VectorND(0, 0)
+        self.v2 = fastvector.VectorND(-1, 1)
+        self.v3 = fastvector.VectorND(2.5, -2.5)
 
     def test_equality(self):
         """ Tests the equality operator.
         """
         self.assertNotEqual(self.v1, self.v2)
-        expected_result = Vector2D(-1, 1)
+        expected_result = fastvector.VectorND(-1, 1)
         self.assertEqual(self.v2, expected_result)
 
     def test_add(self):
         """ Tests the addition operator.
         """
         result = self.v1 + self.v2
-        expected_result = Vector2D(-1, 1)
+        expected_result = fastvector.VectorND(-1, 1)
         self.assertEqual(result, expected_result)
 
     def test_sub(self):
         """ Tests the subtraction operator.
         """
         result = self.v2 - self.v3
-        expected_result = Vector2D(-3.5, 3.5)
+        expected_result = fastvector.VectorND(-3.5, 3.5)
         self.assertEqual(result, expected_result)
 
     def test_mul(self):
         """ Tests the multiplication operator.
         """
         result1 = self.v1 * 5
-        expected_result1 = Vector2D(0.0, 0.0)
+        expected_result1 = fastvector.VectorND(0.0, 0.0)
         self.assertEqual(result1, expected_result1)
         result2 = self.v1 * self.v2
         expected_result2 = 0.0
@@ -45,8 +45,25 @@ class VectorTests(unittest.TestCase):
         """ Tests the multiplication operator.
         """
         result = self.v3 / 5
-        expected_result = Vector2D(0.5, -0.5)
+        expected_result = fastvector.VectorND(0.5, -0.5)
         self.assertEqual(result, expected_result)
+
+    def test_python_clip(self):
+        min_value = -1
+        max_value = 1
+        result = fastvector.VectorND(0, 0)
+        fastvector.python_clip_vector(self.v3, min_value, max_value, result)
+        expected_result = fastvector.VectorND(1, -1)
+        self.assertEqual(result, expected_result)
+
+    def test_cython_clip(self):
+        min_value = -1
+        max_value = 1
+        result = fastvector.VectorND(0, 0)
+        fastvector.cython_clip_vector(self.v3, min_value, max_value, result)
+        expected_result = fastvector.VectorND(1, -1)
+        self.assertEqual(result, expected_result)
+
 
 if __name__ == "__main__":
     unittest.main()
