7f27a457e70d2b9e1adb0ec2613e995905202fb5
Jan Schaffranek <jan.schaffranek@rub.de>
2020-04-27 16:10:39 +0200

    add

--- Chapter5_Functions/Functions/command_line_arguments3.py
+++ Chapter5_Functions/Functions/command_line_arguments3.py
@@ -1,10 +1,17 @@
 import argparse
 
+def check_for_boolean_value(val):
+    if val == "True":
+        return True
+    else:
+        return False
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--age", help="Enter your age (int)", type=int, required=True)
     parser.add_argument("--name", help="Enter your name (str)", type=str, required=True)
-    parser.add_argument("--admin", help="Are your an admin? (bool)", type=bool, required=False)
+    parser.add_argument("--admin", help="Are your an admin? (bool)", 
+                        type=check_for_boolean_value, required=False, default=False)
     args = parser.parse_args()
     age = args.age
     name = args.name
