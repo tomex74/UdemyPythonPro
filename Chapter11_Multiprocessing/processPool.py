8277908f7c0599d97d89595d7d28b003f09acb06
Jan Schaffranek <jan.schaffranek@rub.de>
2020-05-14 23:24:18 +0200

    add

--- Chapter11_Multiprocessing/processPool.py
+++ Chapter11_Multiprocessing/processPool.py
@@ -1,7 +1,7 @@
 import math
 import time
 
-import concurrent.futures
+from multiprocessing import Pool
 
 NUM_PROCESSES = 4
 
@@ -17,11 +17,10 @@ def calc(upper_bound=7_000_000):
 def main():
     start_time = time.perf_counter()
 
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        results = [executor.submit(calc, 7_000_000) for _ in range(NUM_PROCESSES)]
-    
-    for fn in concurrent.futures.as_completed(results):
-        print(fn.result())
+    with Pool(processes=NUM_PROCESSES) as pool:
+        args = [7_000_000 for _ in range(NUM_PROCESSES)]
+        results = list(pool.map(calc, args))
+        print(results)
 
     end_time = time.perf_counter()
     print("took: {} s".format(end_time - start_time))
